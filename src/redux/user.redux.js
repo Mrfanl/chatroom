17cadca7d13b3b97a95800f614f7651afa7f1fe4
fanl <fanliang5959@sina.com>
2019-03-06 14:45:34 +0800

    reudx的相关代码

---
+++ src/redux/user.redux.js
@@ -0,0 +1,54 @@
+import axios from 'axios';
+
+const REGISTER_SUCESS = 'REGISTER_SUCESS';
+const ERROR_MSG = 'ERROR_MSG';
+
+const initState = {
+  redirectTo:'',
+  isAuth:'',
+  msg:'',
+  user:'',
+  gender:'',
+  password:''
+}
+
+export function User(state=initState,action){
+  switch(action.type){
+    case REGISTER_SUCESS:
+        return {...state,isAuth:true,redirectTo:'/login',...action.payload}
+    case ERROR_MSG:
+        return {...state,isAuth:false,msg:action.payload}
+    default:
+    return state
+
+  }
+}
+
+function register_sucess(data){
+  return {type:REGISTER_SUCESS,payload:data};
+}
+
+function errorMsg(data){
+  return {type:ERROR_MSG,payload:data};
+}
+
+export function register({user,gender,password,repassword}){
+  if(!user||!password||!repassword){
+    return errorMsg('用户名密码不能为空');
+  }
+
+  if(password!==repassword){
+    return errorMsg('密码不一致');
+  }
+  return dispatch =>{
+    axios.post('/user/register',{user,gender,password,repassword})
+    .then(res=>{
+      if(res.status===200&&res.data.code===0){
+        dispatch(register_sucess(res.data.data))
+      }else{
+        dispatch(errorMsg(res.data.msg))
+      }
+     }
+    )
+  }
+}
