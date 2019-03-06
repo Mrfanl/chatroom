44d496374fb167f8f6dac022ca1d73ff3d630eb5
fanl <fanliang5959@sina.com>
2019-03-06 14:44:49 +0800

    增加了一些路由设置

--- src/index.js
+++ src/index.js
@@ -3,8 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore,applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { BrowserRouter,Route,Switch,Redirect } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Register from './register/register';
+import Login from './login/login';
+import reducers from './reducers';
+
+const store = createStore(reducers,applyMiddleware(thunk));
+
+ReactDOM.render(
+  (<Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+        <Route path='/register' component={ Register }/>
+        <Route path='/login' component={ Login }/>
+      </Switch>
+    </BrowserRouter>
+   </Provider>
+  )
+  , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
