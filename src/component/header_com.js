e6b41f6c662bab155bbfcc3447abc8f763578377
fanl <fanliang5959@sina.com>
2019-03-07 14:42:09 +0800

    增加了页面头部和底部的css设置

--- src/component/header_com.js
+++ src/component/header_com.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Menu,Icon } from 'antd';
+import { Input,Icon,Button } from 'antd';
 
 import '../static/layout.css';
 
 
-const MenuItemGroup = Menu.ItemGroup;
+const Search = Input.Search;
 
 class Header_Com extends React.Component{
   constructor(props){
@@ -16,10 +16,18 @@ class Header_Com extends React.Component{
   render(){
     return(
       <ul className="header-ul">
-      <li className="header-li">     <span>ChatRoom</span></li>
-      <li className="header-li">     <a href="#1"><Icon type='home'/>主页  </a></li>
-      <li className="header-li">     <a href="#2"><Icon type='team'/>聊天室   </a></li>
-      <li className="header-li">     <a href="#3"><Icon type='user'/>个人中心  </a> </li>
+        <li className="header-li">  <span>ChatRoom</span></li>
+        <li className="header-li">  <a href="#1"><Icon type='home'/>主页  </a></li>
+        <li className="header-li">  <a href="#2"><Icon type='team'/>聊天室   </a></li>
+        <li className="header-li">  <a href="#3"><Icon type='user'/>个人中心  </a> </li>
+        <li className="header-search">
+        <Search
+         placeholder="input search text"
+         style={{width:200}}/>
+        </li>
+        <li className="header-li">
+           <a href="/register"><Button ghost>注册</Button></a>
+        </li>
      </ul>
     );
   }
