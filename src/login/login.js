52189b1f3f6469fa9ea83421ec2454b6b9a5fbc2
fanl <fanliang5959@sina.com>
2019-03-07 14:44:17 +0800

    增加了前端页面的登录逻辑

--- src/login/login.js
+++ src/login/login.js
@@ -1,23 +1,78 @@
 import React from 'react';
+import { Button,Card,Input,Icon} from 'antd';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
 import Header_Com from '../component/header_com';
+import Footer_Com from '../component/footer_com';
+import { login } from '../redux/user.redux';
+import '../static/layout.css';
 
 class Login extends React.Component{
   constructor(props){
     super(props);
     this.state={
-
+      user:'',
+      password:''
     }
+  }
+
+  handleChange=(d,v)=>{
+    this.setState({
+      [d]:v
+    });
+  }
+
+  handleLogin=()=>{
+    this.props.login(this.state);
   }
 
   render(){
     return(
       <div>
+      {this.props.redirectTo?<Redirect to={this.props.redirectTo}/>:null}
       <Header_Com/>
-        <h3>登录界面</h3>
+        <div className="login-content">
+        <ul className="login-ul">
+          <li
+          className="login-li"
+
+          >
+
+          </li>
+          <li className="login-li">
+            <Card
+            title="登录页面"
+            style={{'width':'70%'}}
+            >
+            <p className="msg">{this.props.msg}</p>
+            <p>用户名</p>
+            <Input
+            prefix={ <Icon type="user" style={{ color:'#08c' }} /> }
+            placeholder="username"
+            onChange={e=>this.handleChange("user",e.target.value)}
+            />
+            <p/>
+            <p>密码</p>
+            <Input.Password
+            prefix={<Icon type="lock" style={{ color: '#08c' }} />}
+            placeholder="Password"
+            onChange={e=>this.handleChange("password",e.target.value)}
+            />
+            <p/>
+            <p>
+            <Button type="primary" style={{'marginLeft':'20px','marginRight':'20px','marginTop':'2em'}} onClick={()=>this.handleLogin()}>登录</Button>
+            <a href="/register">未注册？点击这里注册</a></p>
+            </Card>
+          </li>
+        </ul>
+        </div>
+        <Footer_Com/>
       </div>
     )
   }
 }
+
+Login = connect(state=>state.User,{ login })(Login);
 
 export default Login;
--- src/redux/user.redux.js
+++ src/redux/user.redux.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const REGISTER_SUCESS = 'REGISTER_SUCESS';
+const LOGIN_SUCESS = 'LOGIN_SUCESS';
 const ERROR_MSG = 'ERROR_MSG';
 
 const initState = {
@@ -15,9 +16,11 @@ const initState = {
 export function User(state=initState,action){
   switch(action.type){
     case REGISTER_SUCESS:
-        return {...state,isAuth:true,redirectTo:'/login',...action.payload}
+        return {...state,isAuth:true,redirectTo:'/login',...action.payload};
     case ERROR_MSG:
-        return {...state,isAuth:false,msg:action.payload}
+        return {...state,isAuth:false,msg:action.payload};
+    case LOGIN_SUCESS:
+        return {...state,isAuth:true,redirectTo:'/',...action.payload};
     default:
     return state
 
@@ -26,6 +29,10 @@ export function User(state=initState,action){
 
 function register_sucess(data){
   return {type:REGISTER_SUCESS,payload:data};
+}
+
+function login_sucess(data){
+  return {type:LOGIN_SUCESS,payload:data};
 }
 
 function errorMsg(data){
@@ -45,6 +52,24 @@ export function register({user,gender,password,repassword}){
     .then(res=>{
       if(res.status===200&&res.data.code===0){
         dispatch(register_sucess(res.data.data))
+      }else{
+        dispatch(errorMsg(res.data.msg))
+      }
+     }
+    )
+  }
+}
+
+export function login({user,password}){
+  if(!user||!password){
+    return errorMsg('用户名密码不能为空');
+  }
+
+  return dispatch =>{
+    axios.post('/user/login',{user,password})
+    .then(res=>{
+      if(res.status===200&&res.data.code===0){
+        dispatch(login_sucess(res.data.data))
       }else{
         dispatch(errorMsg(res.data.msg))
       }
